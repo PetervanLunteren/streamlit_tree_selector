5a3d9ae0cb301049a56b37e24289caa99265f888
BlueGob <moslemgannoun@gmail.com>
2025-02-02 08:57:52 +0100

    setup update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-tree-select",
-    version="0.0.5",
-    author="Schluca",
+    version="0.0.6",
+    author="BlueGob",
     author_email="",
     description="A simple and elegant checkbox tree for Streamlit.",
     long_description="",
     long_description_content_type="text/plain",
-    url="https://github.com/Schluca/streamlit_tree_select",
+    url="https://github.com/BlueGob/streamlit_tree_select",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
